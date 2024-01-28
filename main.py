111a114e787e09baf4cb7243fdea7526d6f41cd0
abuil1 <a.buil@desigual.com>
2024-01-28 20:38:35 +0100

    main

---
+++ main.py
@@ -0,0 +1,9 @@
+## This script would be used to productivize and execute the main functions of the model:
+
+# STEP 1: preprocessing
+# STEP 2: feature_engineering
+# STEP 3: model_training
+# STEP 4: predictor
+
+
+
